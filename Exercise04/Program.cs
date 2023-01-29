31e9180c1235644e53334eefe75b63f6f9e9daa2
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-29 13:36:29 +0300

    Задание 4. Начало

---
+++ Exercise04/Program.cs
@@ -0,0 +1,45 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+Напишите программу по поиску заданного элемента в случайном массиве.
+*/
+
+//Метод с указанием автора
+void Author()
+{
+    Console.WriteLine();
+    Console.WriteLine("Программа создана Te3Ka_PaynE.");
+    Console.WriteLine("E-mail: Mnement4813@yandex.ru");
+    Console.WriteLine();
+}
+
+int[] GenerateArray()
+{
+    int[] newArray = new int[123];
+    Random rand = new Random();
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        newArray[i] = rand.Next(0, 1000);
+    }
+    return newArray;
+}
+
+void PrintArray(int[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i++)
+    {
+        if (i == (printArray.Length - 1))
+            Console.WriteLine($"{printArray[i]}]");
+        else
+            Console.Write($"{printArray[i]}, ");
+    }
+}
+
+Console.WriteLine("Программа генерирует массив случайных чисел от 0 до 999 в количестве 123 элемента.");
+Console.WriteLine("После производит подсчёт всех чисел в отрезке от 10 до 99");
+int[] array = GenerateArray();
+PrintArray(array);
