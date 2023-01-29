342fe226de5cb51eb73aab46362defe7e35c376e
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-01-29 11:46:06 +0300

    Задание 1. Начало

---
+++ Exercise01/Program.cs
@@ -0,0 +1,17 @@
+﻿/***********************/
+/*****Te3Ka_PaynE*******/
+/*Mnement4813@yandex.ru*/
+/***********************/
+
+/*
+
+*/
+
+//Метод с указанием автора
+void Author()
+{
+    Console.WriteLine();
+    Console.WriteLine("Программа создана Te3Ka_PaynE.");
+    Console.WriteLine("E-mail: Mnement4813@yandex.ru");
+    Console.WriteLine();
+}
